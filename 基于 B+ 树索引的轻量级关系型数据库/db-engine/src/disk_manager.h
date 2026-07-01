2540aae7272a45cad0047010562587dbf23617fb
weihaocheng1 <775022151@qq.com>
2026-07-01 22:29:44 +0800

    Rename disk manager API functions for consistency

--- 基于 B+ 树索引的轻量级关系型数据库/db-engine/src/disk_manager.h
+++ 基于 B+ 树索引的轻量级关系型数据库/db-engine/src/disk_manager.h
@@ -4,19 +4,25 @@
 #include <stdio.h>
 #include "types.h"
 
+// 磁盘管理器结构体
 typedef struct {
-    FILE* fd;
-    char* filename;
-    uint32_t num_pages;
-    uint8_t bitmap[BITMAP_SIZE];
+    FILE*     fd;
+    char*     filename;
+    uint32_t  num_pages;
+    uint8_t   bitmap[BITMAP_SIZE];
 } DiskManager;
 
-DBStatus dm_open(DiskManager** dm, const char* filename);
-DBStatus dm_close(DiskManager* dm);
-DBStatus dm_read_page(DiskManager* dm, page_id_t page_id, Page* page);
-DBStatus dm_write_page(DiskManager* dm, page_id_t page_id, const Page* page);
-page_id_t dm_allocate_page(DiskManager* dm);
-DBStatus dm_free_page(DiskManager* dm, page_id_t page_id);
+// 对外API
+DBStatus db_open(DiskManager** dm, const char* filename);
+DBStatus db_close(DiskManager* dm);
+DBStatus db_read_page(DiskManager* dm, page_id_t page_id, Page* page);
+DBStatus db_write_page(DiskManager* dm, page_id_t page_id, const Page* page);
+page_id_t db_allocate_page(DiskManager* dm);
+DBStatus db_free_page(DiskManager* dm, page_id_t page_id);
 uint32_t dm_get_num_pages(DiskManager* dm);
+
+// 内部静态工具函数声明
+static DBStatus dm_load_header(DiskManager* dm);
+static DBStatus dm_save_header(DiskManager* dm);
 
 #endif
